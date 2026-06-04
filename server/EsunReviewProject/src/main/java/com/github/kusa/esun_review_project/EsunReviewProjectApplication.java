924980553d50ff8cd94deffdd7fc376b0d1d64e9
NazonoKusa <nazonokusa0706@gmail.com>
2026-06-05 01:15:32 +0800

    init

---
+++ server/EsunReviewProject/src/main/java/com/github/kusa/esun_review_project/EsunReviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.kusa.esun_review_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsunReviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsunReviewProjectApplication.class, args);
+	}
+
+}
---
+++ server/EsunReviewProject/src/test/java/com/github/kusa/esun_review_project/EsunReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kusa.esun_review_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsunReviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
